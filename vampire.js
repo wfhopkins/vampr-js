41cb4cf47f634c2490134fbe87365deb74edf3bf
wfhopkins <will.hop@gmail.com>
2023-06-16 17:02:17 -0700

    non stretch functionality implemented

--- vampire.js
+++ vampire.js
@@ -5,29 +5,35 @@ class Vampire {
     this.offspring = [];
     this.creator = null;
   }
-
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOfVamps = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numOfVamps++
+    }
+    return numOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
   }
-
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -39,6 +45,14 @@ class Vampire {
 
   }
 }
+
+const rootVampire = new Vampire("rootVampire", "1476")
+const ansel = new Vampire("Ansel", "1520")
+const bart = new Vampire("Bart", "1543")
+const spike = new Vampire("Spike", "1570")
+const elgort = new Vampire("Elgort", "1616")
+const sarah = new Vampire("Sarah", "1625")
+const andrew = new Vampire("Andrew", "1840")
 
 module.exports = Vampire;
 
